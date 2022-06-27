7f9a7176f704097438757f370a43557eac9e4180
MarcioRCH <marciorchsilva@gmail.com>
2022-06-27 17:04:13 -0300

    Update Individual

--- src/entities/Individual.java
+++ src/entities/Individual.java
@@ -2,9 +2,33 @@ package entities;
 
 public class Individual extends TaxPayer{
 
+	private Double helthExpenditures;
 	
+	public Individual() {
+		super();
+	}
+	public Individual(String name, Double anualIncome, Double helthExpenditures) {
+		super(name, anualIncome);
+		this.helthExpenditures = helthExpenditures;
+	}
+	public Double getHelthExpenditures() {
+		return helthExpenditures;
+	}
+	public void setHelthExpenditures(Double helthExpenditures) {
+		this.helthExpenditures = helthExpenditures;
+	}
 	@Override
 	public Double tax() {
-		return null;
+		double tax = 0.0;
+		if(anualIncome < 20000.00) {
+			tax = anualIncome * 0.15;
+		}else if(anualIncome > 20000.00) {
+			tax = anualIncome * 0.25;
+		}else if(anualIncome < 20000.00 && helthExpenditures != 0) {
+			tax = (anualIncome * 0.15) - (helthExpenditures * 0.50);
+		}else if(anualIncome > 20000.00 && helthExpenditures != 0) {
+			tax = (anualIncome * 0.25) - (helthExpenditures * 0.50);
+		}
+		return tax;
 	}
 }
